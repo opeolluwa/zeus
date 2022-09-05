3fa5c5fa26e39bd3a37b132a416b90ee16ffd041
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-05 03:30:52 +0100

    CHORE

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 
 //import modules
 mod modules;
+use modules::shell;
 fn main() {
-
+    shell::Zeus::parse();
 }
--- src/modules/shell.rs
+++ src/modules/shell.rs
@@ -1,6 +1,10 @@
-use clap::{Args, Parser, SubCommand};
-
-//define the cli commands parser
+use clap::{Args, Parser, Subcommand};
+/*
+ the cli will take 3 (or more) sub commands
+  - auth - to authorize user login and sigh up
+  - config - to configure a user interface
+  - chat - to create a chat id or connect to one
+ */
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 pub struct Zeus {
@@ -8,20 +12,15 @@ pub struct Zeus {
     pub command: ZeusSubCommands,
 }
 
-/**
- * the cli will take 3 (or more) sub commands
- * - auth - to authorize user login and sigh up
- * - config - to configure a user interface
- * - chat - to create a chat id or connect to one
- */
+
 #[derive(clap::Subcommand, Debug)]
 pub enum ZeusSubCommands {
-    ///authorize a user
+    ///create account or login
     Auth(AuthCommands),
-    //configure the color, typeface, email back up e.t.c
+    ///configure the color, typeface, email back up e.t.c
     Config(ConfigCommands),
-    //connect to a message Id
-    Chat(ConnectCommands),
+    ///begin a conversation, send invites or join a chat
+    Chat(ChatCommands),
 }
 
 /*
@@ -30,22 +29,23 @@ pub enum ZeusSubCommands {
 */
 #[derive(Args, Debug)]
 pub struct AuthCommands {
-    auth_sub_commands: AuthSubCommands,
+    #[clap(subcommand)]
+    pub auth_sub_commands: AuthSubCommands,
 }
 
 /** the authorization sub commands  */
 #[derive(clap::Subcommand, Debug)]
 pub enum AuthSubCommands {
     ///takes in username, and password, creates a new user
-    SignUpCommand(AuthSignUp),
+    Register(AuthSignUp),
     /// takes in username and password, logs a user in to the zeus organization
-    LoginCommand(AuthLogin),
+    Login(AuthLogin),
 }
 
 //implementation of the sigh up sub commands
 #[derive(Args, Debug)]
 pub struct AuthSignUp {
-     /// the username
+    /// the username
     #[clap(short, long, value_parser, forbid_empty_values = true)]
     pub username: String,
     /// the user password
@@ -55,7 +55,7 @@ pub struct AuthSignUp {
 
 //implementation of the login commands
 #[derive(Args, Debug)]
-struct AuthLogin {
+pub struct AuthLogin {
     /// the username
     #[clap(short, long, value_parser, forbid_empty_values = true)]
     pub username: String,
@@ -64,11 +64,43 @@ struct AuthLogin {
     pub password: String,
 }
 
-
-
 #[derive(Args, Debug)]
-pub struct ConnectCommands {}
+pub struct ChatCommands {
+    #[clap(subcommand)]
+    chat_sub_commands: ChatSubCommands
+}
 
+//chat sub commands
+#[derive(Subcommand, Debug)]
+pub enum ChatSubCommands{
+    ///generate a chat id for others to join
+    BeginChatCommand(BeginChat),
+    ///join a chat via id 
+    JoinChatCommand(JoinChat),
+    /// send chat invitation via email
+    SendChatInvitation(SendChatInvitation)
+}
+
+// begin chat
+#[derive(Args, Debug)]
+pub struct  BeginChat{
+    pub chat_id:String,
+}
+
+// begin chat
+#[derive(Args, Debug)]
+pub struct  JoinChat{
+    //TODO: #[derive(serde(rename="chatId"))]
+    /// the chat id 
+    pub chat_id:String,
+}
+
+///share chat invitaion via email
+#[derive(Args, Debug)]
+pub struct  SendChatInvitation{
+    /// the chat id 
+    pub chat_id:String,
+}
 //config commands
 #[derive(Args, Debug)]
 pub struct ConfigCommands {}
