d19f5769190e7304317b59720fbd3252ba946f7d
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-09-05 10:19:41 +0100

    UPDATES -> implemented signup and login

--- src/commands/auth.rs
+++ src/commands/auth.rs
@@ -1,6 +1,8 @@
 use crate::shell::{AuthSubCommands, User};
 use crate::{config, shell::AuthCommands};
-use dialoguer::{Input, Password};
+use bcrypt::{hash, verify, DEFAULT_COST};
+use console::style;
+use dialoguer::{theme::ColorfulTheme, Input, Password};
 use mongodb::bson::doc;
 
 pub async fn handle_authorization(auth_command: AuthCommands) {
@@ -9,7 +11,7 @@ pub async fn handle_authorization(auth_command: AuthCommands) {
 
     //exec the matching sub command
     match auth_sub_commands {
-        AuthSubCommands::SignUp => sign_up(),
+        AuthSubCommands::SignUp => sign_up().await,
         AuthSubCommands::Login => login().await,
     }
 }
@@ -17,16 +19,10 @@ pub async fn handle_authorization(auth_command: AuthCommands) {
 ///accept username and password from cli and log in a user
 async fn login() {
     //assign value
-    let username: String = Input::new()
-        .with_prompt("username? ")
+    let username: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("Username: ")
         .interact_text()
         .unwrap();
-
-    let password: String = Password::new()
-        .with_prompt("password? ")
-        .interact()
-        .unwrap();
-    // println!(" username {}, password {}", username, password);
 
     //connect to database, check if user details is valid
     let database = config::database::mongodb().await;
@@ -37,21 +33,83 @@ async fn login() {
         .await
         .unwrap();
 
-    //try to destructure the found object
-     if let Some(User {
-        username, password, ..
+    //try to destructure the found object, then ask for password
+    if let Some(User {
+        username,
+        password: hashed_password,
+        ..
     }) = result
     {
-        // (username, password)
-    println!(" username {}, password {}", username, password);
+        let password: String = Password::with_theme(&ColorfulTheme::default())
+            .with_prompt("Password: ")
+            .interact()
+            .unwrap();
 
+        //check for correctness of the pasword
+        let is_correct_password = verify(&password, &hashed_password);
+        match is_correct_password {
+            Ok(_) => println!(
+                "
+                
+                {} {}",
+                style("successfully logged in as").green(),
+                &username
+            ),
+            Err(_) => println!("{} {}", style("incorrect password for ").red(), &username),
+        }
+        println!(
+            " username {}, password {:?}, hash {}",
+            username, password, &hashed_password
+        );
     } else {
         //if no user was found return 404 error
-        println!("User with provided credentials not found");
+        println!(
+            "{}",
+            style("User with provided credentials not found").red()
+        );
         return;
     };
 
-    //generate jwt fo for the user 
+    //generate jwt fo for the user
 }
 
-pub fn sign_up() {}
+pub async fn sign_up() {
+    let username: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("Username: ")
+        .interact_text()
+        .unwrap();
+
+    let password: String = Password::with_theme(&ColorfulTheme::default())
+        .with_prompt("Password ")
+        .interact()
+        .unwrap();
+
+    let database = config::database::mongodb().await;
+    let collection = database.collection::<User>("user_information");
+
+    //see if user already exists
+    let user_already_exists = collection
+        .find_one(doc! {"username:":&username}, None)
+        .await
+        .unwrap();
+
+    if let Some(_) = user_already_exists {
+        println!(
+            "{}",
+            style("A user with the provided name already exist").red()
+        );
+        return;
+    } else {
+        //create a new user
+        let hashed_password =
+            hash(&password, DEFAULT_COST).expect("expected a password but gon an empty string");
+        let user = User {
+            username,
+            password: hashed_password,
+        };
+
+        //save the new user
+        collection.insert_one(&user, None).await.unwrap();
+        println!("{}", style("User account successfully created").green());
+    }
+}
